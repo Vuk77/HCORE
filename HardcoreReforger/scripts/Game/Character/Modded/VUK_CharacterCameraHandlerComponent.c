63daa347e5ba10c725d0fb819852b45f3fc659e3
Ognjen Letić <75700515+Vuk77@users.noreply.github.com>
2023-12-07 17:39:12 +0100

    Add files via upload

--- HardcoreReforger/scripts/Game/Character/Modded/VUK_CharacterCameraHandlerComponent.c
+++ HardcoreReforger/scripts/Game/Character/Modded/VUK_CharacterCameraHandlerComponent.c
@@ -12,9 +12,17 @@ modded class SCR_CharacterCameraHandlerComponent
         if(m_OwnerCharacter.IsInVehicle())
         {
             CompartmentAccessComponent compartmentAccess = m_OwnerCharacter.GetCompartmentAccessComponent();
-            IEntity vehicle = compartmentAccess.GetVehicleIn(m_OwnerCharacter);
+			IEntity vehicle = compartmentAccess.GetVehicleIn(m_OwnerCharacter);
+			BaseCompartmentSlot currentCompartment = compartmentAccess.GetCompartment();
 			
             if(!vehicle.GetPrefabData().GetPrefabName().Contains("BTR70"))
+            {
+				SetThirdPerson(false);
+				
+				return CharacterCameraSet.CHARACTERCAMERA_1ST_VEHICLE;
+            }
+			
+			if(currentCompartment && currentCompartment.GetType() != ECompartmentType.Pilot)
             {
 				SetThirdPerson(false);
 				
