91e142df988a6bfa6402720a458770fb9070a470
Ognjen Letić <75700515+Vuk77@users.noreply.github.com>
2023-12-08 15:13:39 +0100

    Add files via upload

---
+++ HardcoreReforger/scripts/Game/GameMode/Respawn/Modded/VUK_PlayerSpawnPoint.c
@@ -0,0 +1,13 @@
+modded class SCR_PlayerSpawnPoint: SCR_SpawnPoint
+{
+	//------------------------------------------------------------------------------------------------
+	override protected void ActivateSpawnPoint()
+	{
+		
+	}
+	
+	override protected void DeactivateSpawnPoint()
+	{
+		
+	}
+};
